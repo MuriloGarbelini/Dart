d689ae0019155e285bbd5ccc0ae755e29b780c11
Murilo Paiva <murilopaivagg@gmail.com>
2026-03-26 21:54:20 -0300

    2

--- Projeto_Dart/atividades/atividade2.dart
+++ Projeto_Dart/atividades/atividade2.dart
@@ -4,14 +4,12 @@ void main() {
   stdout.write('Digite um número: ');
   int n = int.parse(stdin.readLineSync()!);
 
-  // Par ou ímpar
   if (n % 2 == 0) {
     print('O número é par.');
   } else {
     print('O número é ímpar.');
   }
 
-  // Positivo ou negativo
   if (n > 0) {
     print('O número é positivo.');
   } else if (n < 0) {
